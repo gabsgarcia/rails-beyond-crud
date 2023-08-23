d1da932e638acdb9ab38d6ef251778ce28c1b65f
gabriella garcia <gabirvargas@gmail.com>
2023-08-23 12:19:39 -0300

    reviews on show page of restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants
+  resources :restaurants do
+    # GET /restaurants/:id/reviews/new
+    resources :reviews, only: [:new, :create]
+    collection do
+      # index customizado
+      # /restaurants/qualquer_coisa
+      # get "top"
+      get :top
+    end
+
+    member do
+      # show customizado
+      # /restaurants/:id/chef
+      get :chef
+    end
+  end
+  # GET /reviews
+  # resources: reviews
+  resources :reviews, only: [:show, :edit, :destroy]
 end
